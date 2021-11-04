f28c1f151fd3379625e2eadd1f1ec0ae4f46fe8d
KotoriPana <KotoriPana@gmail.com>
2021-11-04 22:08:10 +0700

    add token auth to Users

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,20 +15,20 @@ router.post("/login", login);
 // router.post("/users", addUser);
 router.get("/users", getUsers);
 router.get("/users/:id", getUser);
-router.patch("/users/:id", updateUser);
-router.delete("/users/:id", deleteUser);
+router.patch("/users/:id", auth, updateUser);
+router.delete("/users/:id", auth, deleteUser);
 
 router.post("/countries", auth, addCountry);
 router.get("/countries", getCountries);
 router.get("/countries/:id", getCountry);
-router.patch("/countries/:id", auth, updateCountry);
-router.delete("/countries/:id", auth, deleteCountry);
+router.patch("/countries/:id", updateCountry);
+router.delete("/countries/:id", deleteCountry);
 
 router.post("/trips", auth, addTrip);
 router.get("/trips", getTrips);
 router.get("/trips/:id", getTrip);
-router.patch("/trips/:id", auth, updateTrip);
-router.delete("/trips/:id", auth, deleteTrip);
+router.patch("/trips/:id", updateTrip);
+router.delete("/trips/:id", deleteTrip);
 
 router.post("/transactions", addTransaction);
 router.get("/transactions", getTransactions);
